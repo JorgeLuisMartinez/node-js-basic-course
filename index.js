54fc22aba9a300a4891fb20c5c58083f59a9ea6d
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-04-14 21:52:28 -0500

    Min:44:04

---
+++ index.js
@@ -0,0 +1,9 @@
+const math = require('./math')
+console.log(math);
+
+/*
+console.log(math.sumar(1, 2));
+console.log(math.restar(1, 2));
+console.log(math.multiplicar(1, 2));
+console.log(math.dividir(1, 2));
+*/
